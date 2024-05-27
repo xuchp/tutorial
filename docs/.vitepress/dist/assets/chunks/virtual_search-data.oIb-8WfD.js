ac4520c758a638a3e9436feb7a9d754720e677e2
xuchp <xucp@51prime.com>
2024-05-27 16:52:25 +0800

    firse build

---
+++ docs/.vitepress/dist/assets/chunks/virtual_search-data.oIb-8WfD.js
@@ -0,0 +1,25 @@
+const e={map:'[{"runtime":["0.0"],"results":["0.1","0.5"],"theme":["0.2","0.6"],"page":["0.3","0.4","0.7","0.8"],"more":["0.9","2.3"],"markdown":["2.0"],"syntax":["2.1"],"custom":["2.2"]},{"api":["0.0"],"examples":["0.0","2.0"],"this":["0.0","2.2"],"page":["0.0"],"demonstrates":["0.0"],"data":["0.2","0.3","0.6","0.7"],"frontmatter":["0.4","0.8"],"check":["0.9","2.3"],"extension":["2.0"],"highlighting":["2.1"],"vitepress":["2.1"],"provides":["2.1"],"containers":["2.2"],"input":["2.2"],"md":["2.2"],"info":["2.2"],"is":["2.2"],"an":["2.2"]},{"usage":["0.0"],"of":["0.0"],"some":["0.0"],"the":["0.0","0.9","2.3"],"pre":["0.2","0.3","0.4","0.6","0.7","0.8"],"out":["0.9","2.3"],"this":["2.0"],"page":["2.0"],"powered":["2.1"],"by":["2.1"],"shiki":["2.1"],"box":["2.2"],"tip":["2.2"],"a":["2.2"],"warning":["2.2"]},{"apis":["0.0"],"provided":["0.0"],"by":["0.0"],"vitepress":["0.0"],"documentation":["0.9","2.3"],"demonstrates":["2.0"],"some":["2.0"],"with":["2.1"],"additional":["2.1"],"features":["2.1"],"like":["2.1"],"line":["2.1"],"danger":["2.2"],"dangerous":["2.2"]},{"main":["0.0"],"usedata":["0.0"],"can":["0.0"],"be":["0.0"],"for":["0.9","2.3"],"of":["2.0"],"the":["2.0"],"input":["2.1"],"md":["2.1"],"js":["2.1"],"details":["2.2"],"block":["2.2"],"output":["2.2"]},{"4":["2.1"],"used":["0.0"],"to":["0.0"],"access":["0.0"],"site":["0.0"],"theme":["0.0"],"and":["0.0"],"full":["0.9","2.3"],"list":["0.9","2.3"],"built":["2.0"],"in":["2.0"],"export":["2.1"],"default":["2.1"],"data":["2.1"]},{"data":["0.0"],"for":["0.0"],"current":["0.0"],"of":["0.9","2.3"],"extensions":["2.0"],"return":["2.1"],"msg":["2.1"],"highlighted":["2.1"],"output":["2.1"]},{"it":["0.0"],"works":["0.0"],"in":["0.0"],"both":["0.0"],"md":["0.0"],"runtime":["0.9"],"apis":["0.9"],"provided":["2.0"],"by":["2.0"],"markdown":["2.3"],"extensions":["2.3"]},{"vue":["0.0"],"files":["0.0"],"vitepress":["2.0"],"asdas":["2.2"],"da":["2.2"]}]'},t={"0.0":{t:"# Runtime API Examples",p:`This page demonstrates usage of some of the runtime APIs provided by VitePress.
+The main useData() A ...`,l:"api-examples.html",a:"runtime-api-examples"},"0.1":{t:"Results",p:"",l:"api-examples.html#results",a:"results"},"0.2":{t:"Theme Data",p:`&lt;pre&gt;{{ theme }}&lt;/pre&gt;
+`,l:"api-examples.html#theme-data",a:"theme-data"},"0.3":{t:"Page Data",p:`&lt;pre&gt;{{ page }}&lt;/pre&gt;
+`,l:"api-examples.html#page-data",a:"page-data"},"0.4":{t:"Page Frontmatter",p:`&lt;pre&gt;{{ frontmatter }}&lt;/pre&gt;
+
+
+
+`,l:"api-examples.html#page-frontmatter",a:"page-frontmatter"},"0.5":{t:"Results",p:"",l:"api-examples.html#results",a:"results"},"0.6":{t:"Theme Data",p:`&lt;pre&gt;{{ theme }}&lt;/pre&gt;
+`,l:"api-examples.html#theme-data",a:"theme-data"},"0.7":{t:"Page Data",p:`&lt;pre&gt;{{ page }}&lt;/pre&gt;
+`,l:"api-examples.html#page-data",a:"page-data"},"0.8":{t:"Page Frontmatter",p:`&lt;pre&gt;{{ frontmatter }}&lt;/pre&gt;
+`,l:"api-examples.html#page-frontmatter",a:"page-frontmatter"},"0.9":{t:"More",p:`Check out the documentation for the full list of runtime APIs.
+`,l:"api-examples.html#more",a:"more"},"2.0":{t:"# Markdown Extension Examples",p:`This page demonstrates some of the built-in markdown extensions provided by VitePress.
+`,l:"markdown-examples.html",a:"markdown-extension-examples"},"2.1":{t:"Syntax Highlighting",p:"VitePress provides Syntax Highlighting powered by Shiki, with additional features like line-highligh ...",l:"markdown-examples.html#syntax-highlighting",a:"syntax-highlighting"},"2.2":{t:"Custom Containers",p:`Input
+::: info
+This is an info box.
+:::
+
+::: tip
+This is a tip.
+:::
+
+::: warning
+This is a warning.
+ ...`,l:"markdown-examples.html#custom-containers",a:"custom-containers"},"2.3":{t:"More",p:`Check out the documentation for the full list of markdown extensions.
+`,l:"markdown-examples.html#more",a:"more"}},a={previewLength:100,buttonLabel:"搜索",placeholder:"请输入关键词",allow:[],ignore:[]},n={INDEX_DATA:e,PREVIEW_LOOKUP:t,Options:a};export{n as default};
